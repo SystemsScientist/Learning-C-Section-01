2d64eca9f10c24725f3dca0ac3a724bb1dede3e4
Matthew Johnson <matthewjohnson@snarkydata.com>
2023-04-29 06:49:19 -0500

    Adding files and directories for Section 01

---
+++ hello_world/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+    printf("Hello world!\n");
+    printf("This file works!\n");
+
+    return 0;
+}
---
+++ sending_text_to-output/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+    puts("This file works!");
+
+    return 0;
+}
